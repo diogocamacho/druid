a89107b5c84cc79aba471b4933f4e613e8f5766f
Diogo Camacho <diogo.camacho.2008@gmail.com>
2019-03-07 23:44:50 -0500

    - corrected description text on some of the parameters.

--- R/druid_score.R
+++ R/druid_score.R
@@ -5,9 +5,9 @@
 #' @param similarity_results Vector of cosine similarities
 #' @param gs_size Size of the query set
 #' @param num_sets Number of random sets to generate
-#' @param target_tfidf Pathway tf-idf matrix.
-#' @param tfidf_crossprod_mat Cross-product matrix for pathway tf-idf.
-#' @return A vector of random probabilities for each pathway given the gene set size.
+#' @param target_tfidf Drug tf-idf matrix.
+#' @param tfidf_crossprod_mat Cross-product matrix for drug tf-idf.
+#' @return A vector of random probabilities for each drug given the gene set size.
 druid_score <- function(similarity_results, random_probabilities, num_random) {
   score <- 1 + (similarity_results / max(similarity_results))  + (-log10(random_probabilities))
   score[random_probabilities == 0] <- 1 + (similarity_results[random_probabilities == 0] / max(similarity_results)) - log10(1 / num_random)
