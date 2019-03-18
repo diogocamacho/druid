66ccfaf09a7b708b230d2016abe13a5defa7b2a9
Diogo Camacho <diogocamacho@mordor.private.wireless.med.harvard.edu>
2019-03-18 15:32:54 -0400

    a function to select the data source desired

---
+++ R/compendium_selection.R
@@ -0,0 +1,22 @@
+#' Compendium selection
+#'
+#' This function will make the selection of which data source to use to run DRUID against. All the datasets were generated with the companion package `cauldron`.
+#' 
+#' @return An integer.
+compendium_selection <- function() {
+  message("Available data sets:")
+  message("[1] CMAP")
+  message("[2] LINCS")
+  message("[3] CTD")
+  message("[4] Small molecules")
+  message("[5] Natural products")
+  message("[6] Run all")
+  message("")
+  selection <- readline(promp = "Selection: ")
+  selection <- as.integer(selection)
+  if(selection %in% seq(1, 6)) {
+    return(selection)
+  } else {
+    message("Not an available data set. Stopping.")
+  }
+}
