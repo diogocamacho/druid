a00ff2241aeb3e1a71222191ca51509da0f05d8c
Diogo Camacho <diogocamacho@mordor.private.wireless.med.harvard.edu>
2019-03-18 15:33:17 -0400

    DRUID package documentation file

---
+++ R/druid.R
@@ -0,0 +1,7 @@
+#' DRUID: DRUg Indication Discoverer
+#' 
+#' The DRUID package allows one to identify compounds for mimicking or reverting biological profiles of interest.
+#' 
+#' @docType package
+#' @name DRUID
+NULL
